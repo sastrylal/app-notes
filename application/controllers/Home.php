28af7792284026357bdbd94a1532e2e404bd35ae
L B Sastry <sastrylal@gmail.com>
2021-11-25 03:31:05 +0530

    init setup

---
+++ application/controllers/Home.php
@@ -0,0 +1,27 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends MY_Controller {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        $data = [];
+        redirect("/home/notes");
+        $this->load->view('home', $data);
+    }
+    
+    public function notes() {
+        $data = [];
+        $this->load->view('notes', $data);
+    }
+    
+    public function tags() {
+        $data = [];
+        $this->load->view('tags', $data);
+    }
+
+}
